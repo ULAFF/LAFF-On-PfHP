50939f752186ead91232d4af6afebcdb070765cd
Robert van de Geijn <rvdg@Maggies-MacBook-Air-3.local>
2019-06-15 12:46:57 -0400

    removing output files

--- Assignments/Week4/C/data/output_MT_Loop5_8x6Kernel.m
+++
@@ -1,8 +0,0 @@
-% Number of threads = 4
-
-% number of repeats:% 3
-% enter first, last, inc:% 48 2496 48 
-data = [
-%  n          reference      |         current implementation 
-%        time       GFLOPS   |    time       GFLOPS     diff 
-  2496 1.9743e-01 1.5752e+02   
