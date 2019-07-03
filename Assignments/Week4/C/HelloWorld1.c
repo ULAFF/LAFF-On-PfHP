a8681820ace5040feee4f52710e5a8ca7249182a
Robert van de Geijn <rvdg@Maggies-MacBook-Air-3.local>
2019-07-02 21:17:43 -0500

    removing files

--- Assignments/Week4/C/HelloWorld1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "omp.h"
-
-int main(int argc, char *argv[])
-{
-  printf( "Hello World!\n" );
-}
--- Assignments/Week4/C/HelloWorld2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "omp.h"
-
-int main(int argc, char *argv[])
-{
-  #pragma omp parallel
-  printf( "Hello World!\n" );
-}
